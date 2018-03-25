b523619f70550eaa0c7b7ca1215f7ddfd182d75d
szuzanna <zdrapka111@gmail.com>
2018-03-25 23:04:55 +0200

    Code cleaning up in AbstractEmployee and Employee.

--- src/main/java/bit/javaoop/Employee.java
+++ src/main/java/bit/javaoop/Employee.java
@@ -7,6 +7,12 @@ public class Employee extends AbstractEmployee {
         super(name, salary);
     }
 
+    public void answerQuestions() {
+        System.out.println("What is your name?\n" + "My name is: " + this.getName() + "\n");
+        System.out.println("What is your salary?\n" + "I earn: " + this.getSalary() + "\n");
+        System.out.println("Are you satisfied?\n" + this.isSatisfied() + "\n");
+    }
+
     @Override
     public boolean isSatisfied() {
         return getSalary() > 10_000;
