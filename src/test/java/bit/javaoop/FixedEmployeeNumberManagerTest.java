cccb395d55f604684572822ca492e2b5389e03de
szuzanna <zdrapka111@gmail.com>
2018-03-27 21:11:52 +0200

    Add missing //given //when //then.

--- src/test/java/bit/javaoop/FixedEmployeeNumberManagerTest.java
+++ src/test/java/bit/javaoop/FixedEmployeeNumberManagerTest.java
@@ -10,39 +10,51 @@ class FixedEmployeeNumberManagerTest {
 
     @Test
     void shouldBeSatisfiedWithHighSalaryAndNoVacancyLeft() {
+        // given
         FixedEmployeeNumberManager happy_manager = new FixedEmployeeNumberManager("Jan", 300_000);
         happy_manager.setEmployeeLimit(1);
         Employee employee = new Employee("Zbyszek", 100_000);
         happy_manager.hireNewEmployee(employee);
+        // when
         boolean test_result = happy_manager.isSatisfied();
+        // then
         assertTrue(test_result);
     }
 
     @Test
     void shouldNotBeSatisfiedWithLowSalaryAndNoVacancyLeft() {
+        // given
         FixedEmployeeNumberManager sad_manager = new FixedEmployeeNumberManager("Jan", 3_000);
         sad_manager.setEmployeeLimit(1);
         Employee employee = new Employee("Zbyszek", 100_000);
         sad_manager.hireNewEmployee(employee);
+        // when
         boolean test_result = sad_manager.isSatisfied();
+        // then
         assertFalse(test_result);
     }
 
     @Test
     void succeedInHiringNewEmployee() {
+        // given
         FixedEmployeeNumberManager manager = new FixedEmployeeNumberManager("Jan", 3_000);
         manager.setEmployeeLimit(1);
         Employee employee = new Employee("Zbyszek", 100_000);
+        // when
         manager.hireNewEmployee(employee);
+        // then
         assertTrue(manager.hireNewEmployee(employee));
     }
 
     @Test
     void failedToHireNewEmployee() {
+        // given
         FixedEmployeeNumberManager manager = new FixedEmployeeNumberManager("Jan", 3_000);
         manager.setEmployeeLimit(0);
         Employee employee = new Employee("Zbyszek", 100_000);
+        // when
         manager.hireNewEmployee(employee);
+        // then
         assertFalse(manager.hireNewEmployee(employee));
     }
 }
